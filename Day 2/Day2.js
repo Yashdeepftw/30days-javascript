7c6adecab457501cd3a03c6f0b3ba01063c0dd30
Yashdeep Chauhan <111493305+Yashdeepftw@users.noreply.github.com>
2024-07-14 13:18:04 +0530

    Solutions of Day 2 Tasks

---
+++ Day 2/Day2.js
@@ -0,0 +1,61 @@
+// Day 2
+// Task 1
+let a=10;
+let b=20;
+let sum=a+b;
+console.log("sum of a and b is: ",sum);
+
+// Task 2
+let x=10;
+let y=5;
+let sub=x-y;
+console.log("subtraction of a and b is: ",sub);
+
+// Task 3
+let c=5;
+let d=2;
+let mul=c*d;
+console.log("Multiplication of c and d is: ",mul);
+
+// Task 4
+let e=10;
+let f=2;
+let div=e/f;
+console.log("division of e and f is : ",div);
+
+// Task 5
+let mod=e%f;
+console.log("modulous of e and f is: ",mod);
+
+// Task 6
+console.log(a+=10);
+console.log(a);
+
+// Task 7
+console.log(b-=5);
+
+// Task 8
+console.log(a>b);
+console.log(e<f);
+
+// Tassk 9
+console.log(a>=b);
+console.log(e<=f);
+
+// Task 10
+const str='15';
+let str2=15;
+console.log(str==str2);
+console.log(str===str2);
+
+// Task 11
+console.log(a>b&&c>d);
+
+// Task 12
+console.log(a<b||c>d);
+
+// Task 13
+console.log(!(a>b));
+
+// Task 14
+console.log((a<b)?'true':'false');
---
+++ Day 2/Day2[FR].js
@@ -0,0 +1,25 @@
+// Feature request
+// number 1
+let a=10;
+let b=2;
+let sum =a+b;
+let sub=a-b;
+let mul=a*b;
+let div=a/b;
+let mod=a%b;
+console.log("sum of a and b is: ",sum);
+console.log("sub of a and b is: ",sub);
+console.log("mul of a and b is: ",mul);
+console.log("div of a and b is: ",div);
+console.log("mod of a and b is: ",mod);
+
+// number 2
+let x=20;
+let y=2;
+console.log(x===y||x==y);
+console.log(x>=y||x<=y);
+
+// number 3
+let s=33;
+let f='33';
+console.log((s===f)?'True':'False');
